8943c3bb101dcbccec02ccef986752dc9320f883
ZeqMacaw <zeqmacaw@gmail.com>
2019-11-29 01:44:26 -0500

    Clean up.

--- Crowbar/Core/GameModel/SourceModel44/SourceMdlFileData/SourceMdlFileData44.vb
+++ Crowbar/Core/GameModel/SourceModel44/SourceMdlFileData/SourceMdlFileData44.vb
@@ -9,6 +9,8 @@ Public Class SourceMdlFileData44
 
 		'Me.theMdlFileOnlyHasAnimations = False
 		Me.theFirstAnimationDesc = Nothing
+		Me.theFirstAnimationDescFrameLines = New SortedList(Of Integer, AnimationFrameLine)()
+		Me.theWeightLists = New List(Of SourceMdlWeightList)()
 	End Sub
 
 	'FROM: SourceEngine2006_source\public\studio.h
@@ -23,70 +25,70 @@ Public Class SourceMdlFileData44
 	'	inline const char *	pszName( void ) const { return name; }
 	'	char				name[64];
 	'	int					length;
-
-
+	'
+	'
 	'	Vector				eyeposition;	// ideal eye position
-
+	'
 	'	Vector				illumposition;	// illumination center
-
+	'	
 	'	Vector				hull_min;		// ideal movement hull size
 	'	Vector				hull_max;			
-
+	'
 	'	Vector				view_bbmin;		// clipping bounding box
 	'	Vector				view_bbmax;		
-
+	'
 	'	int					flags;
-
+	'
 	'	int					numbones;			// bones
 	'	int					boneindex;
 	'	inline mstudiobone_t *pBone( int i ) const { Assert( i >= 0 && i < numbones); return (mstudiobone_t *)(((byte *)this) + boneindex) + i; };
 	'	int					RemapSeqBone( int iSequence, int iLocalBone ) const;	// maps local sequence bone to global bone
 	'	int					RemapAnimBone( int iAnim, int iLocalBone ) const;		// maps local animations bone to global bone
-
+	'
 	'	int					numbonecontrollers;		// bone controllers
 	'	int					bonecontrollerindex;
 	'	inline mstudiobonecontroller_t *pBonecontroller( int i ) const { Assert( i >= 0 && i < numbonecontrollers); return (mstudiobonecontroller_t *)(((byte *)this) + bonecontrollerindex) + i; };
-
+	'
 	'	int					numhitboxsets;
 	'	int					hitboxsetindex;
-
+	'
 	'	// Look up hitbox set by index
 	'	mstudiohitboxset_t	*pHitboxSet( int i ) const 
 	'	{ 
 	'		Assert( i >= 0 && i < numhitboxsets); 
 	'		return (mstudiohitboxset_t *)(((byte *)this) + hitboxsetindex ) + i; 
 	'	};
-
+	'
 	'	// Calls through to hitbox to determine size of specified set
 	'	inline mstudiobbox_t *pHitbox( int i, int set ) const 
 	'	{ 
 	'		mstudiohitboxset_t const *s = pHitboxSet( set );
 	'		if ( !s )
 	'			return NULL;
-
+	'
 	'		return s->pHitbox( i );
 	'	};
-
+	'
 	'	// Calls through to set to get hitbox count for set
 	'	inline int			iHitboxCount( int set ) const
 	'	{
 	'		mstudiohitboxset_t const *s = pHitboxSet( set );
 	'		if ( !s )
 	'			return 0;
-
+	'
 	'		return s->numhitboxes;
 	'	};
-
+	'
 	'	// file local animations? and sequences
 	'//private:
 	'	int					numlocalanim;			// animations/poses
 	'	int					localanimindex;		// animation descriptions
 	'  	inline mstudioanimdesc_t *pLocalAnimdesc( int i ) const { if (i < 0 || i >= numlocalanim) i = 0; return (mstudioanimdesc_t *)(((byte *)this) + localanimindex) + i; };
-
+	'
 	'	int					numlocalseq;				// sequences
 	'	int					localseqindex;
 	'  	inline mstudioseqdesc_t *pLocalSeqdesc( int i ) const { if (i < 0 || i >= numlocalseq) i = 0; return (mstudioseqdesc_t *)(((byte *)this) + localseqindex) + i; };
-
+	'
 	'//public:
 	'	bool				SequencesAvailable() const;
 	'	int					GetNumSeq() const;
@@ -94,7 +96,7 @@ Public Class SourceMdlFileData44
 	'	mstudioseqdesc_t	&pSeqdesc( int i ) const;
 	'	int					iRelativeAnim( int baseseq, int relanim ) const;	// maps seq local anim reference to global anim index
 	'	int					iRelativeSeq( int baseseq, int relseq ) const;		// maps seq local seq reference to global seq index
-
+	'
 	'//private:
 	'	mutable int			activitylistversion;	// initialization flag - have the sequences been indexed?
 	'	mutable int			eventsindexed;
@@ -116,17 +118,17 @@ Public Class SourceMdlFileData44
 	'	int					numcdtextures;
 	'	int					cdtextureindex;
 	'	inline char			*pCdtexture( int i ) const { return (((char *)this) + *((int *)(((byte *)this) + cdtextureindex) + i)); };
-
+	'
 	'	// replaceable textures tables
 	'	int					numskinref;
 	'	int					numskinfamilies;
 	'	int					skinindex;
 	'	inline short		*pSkinref( int i ) const { return (short *)(((byte *)this) + skinindex) + i; };
-
+	'
 	'	int					numbodyparts;		
 	'	int					bodypartindex;
 	'	inline mstudiobodyparts_t	*pBodypart( int i ) const { return (mstudiobodyparts_t *)(((byte *)this) + bodypartindex) + i; };
-
+	'
 	'	// queryable attachable points
 	'//private:
 	'	int					numlocalattachments;
@@ -138,7 +140,7 @@ Public Class SourceMdlFileData44
 	'	int					GetAttachmentBone( int i ) const;
 	'	// used on my tools in hlmv, not persistant
 	'	void				SetAttachmentBone( int iAttachment, int iBone );
-
+	'
 	'	// animation node to animation node transition graph
 	'//private:
 	'	int					numlocalnodes;
@@ -146,17 +148,17 @@ Public Class SourceMdlFileData44
 	'	int					localnodenameindex;
 	'	inline char			*pszLocalNodeName( int iNode ) const { Assert( iNode >= 0 && iNode < numlocalnodes); return (((char *)this) + *((int *)(((byte *)this) + localnodenameindex) + iNode)); }
 	'	inline byte			*pLocalTransition( int i ) const { Assert( i >= 0 && i < (numlocalnodes * numlocalnodes)); return (byte *)(((byte *)this) + localnodeindex) + i; };
-
+	'
 	'//public:
 	'	int					EntryNode( int iSequence ) const;
 	'	int					ExitNode( int iSequence ) const;
 	'	char				*pszNodeName( int iNode ) const;
 	'	int					GetTransition( int iFrom, int iTo ) const;
-
+	'
 	'	int					numflexdesc;
 	'	int					flexdescindex;
 	'	inline mstudioflexdesc_t *pFlexdesc( int i ) const { Assert( i >= 0 && i < numflexdesc); return (mstudioflexdesc_t *)(((byte *)this) + flexdescindex) + i; };
-
+	'
 	'	int					numflexcontrollers;
 	'	int					flexcontrollerindex;
 	'	inline mstudioflexcontroller_t *pFlexcontroller( int i ) const { Assert( i >= 0 && i < numflexcontrollers); return (mstudioflexcontroller_t *)(((byte *)this) + flexcontrollerindex) + i; };
@@ -164,15 +166,15 @@ Public Class SourceMdlFileData44
 	'	int					numflexrules;
 	'	int					flexruleindex;
 	'	inline mstudioflexrule_t *pFlexRule( int i ) const { Assert( i >= 0 && i < numflexrules); return (mstudioflexrule_t *)(((byte *)this) + flexruleindex) + i; };
-
+	'
 	'	int					numikchains;
 	'	int					ikchainindex;
 	'	inline mstudioikchain_t *pIKChain( int i ) const { Assert( i >= 0 && i < numikchains); return (mstudioikchain_t *)(((byte *)this) + ikchainindex) + i; };
-
+	'
 	'	int					nummouths;
 	'	int					mouthindex;
 	'	inline mstudiomouth_t *pMouth( int i ) const { Assert( i >= 0 && i < nummouths); return (mstudiomouth_t *)(((byte *)this) + mouthindex) + i; };
-
+	'
 	'//private:
 	'	int					numlocalposeparameters;
 	'	int					localposeparamindex;
@@ -181,40 +183,40 @@ Public Class SourceMdlFileData44
 	'	int					GetNumPoseParameters( void ) const;
 	'	const mstudioposeparamdesc_t &pPoseParameter( int i ) const;
 	'	int					GetSharedPoseParameter( int iSequence, int iLocalPose ) const;
-
+	'
 	'	int					surfacepropindex;
 	'	inline char * const pszSurfaceProp( void ) const { return ((char *)this) + surfacepropindex; }
-
+	'
 	'	// Key values
 	'	int					keyvalueindex;
 	'	int					keyvaluesize;
 	'	inline const char * KeyValueText( void ) const { return keyvaluesize != 0 ? ((char *)this) + keyvalueindex : NULL; }
-
+	'
 	'	int					numlocalikautoplaylocks;
 	'	int					localikautoplaylockindex;
 	'	inline mstudioiklock_t *pLocalIKAutoplayLock( int i ) const { Assert( i >= 0 && i < numlocalikautoplaylocks); return (mstudioiklock_t *)(((byte *)this) + localikautoplaylockindex) + i; };
-
+	'
 	'	int					GetNumIKAutoplayLocks( void ) const;
 	'	const mstudioiklock_t &pIKAutoplayLock( int i ) const;
 	'	int					CountAutoplaySequences() const;
 	'	int					CopyAutoplaySequences( unsigned short *pOut, int outCount ) const;
 	'	int					GetAutoplayList( unsigned short **pOut ) const;
-
+	'
 	'	// The collision model mass that jay wanted
 	'	float				mass;
 	'	int					contents;
-
+	'
 	'	// external animations, models, etc.
 	'	int					numincludemodels;
 	'	int					includemodelindex;
 	'	inline mstudiomodelgroup_t *pModelGroup( int i ) const { Assert( i >= 0 && i < numincludemodels); return (mstudiomodelgroup_t *)(((byte *)this) + includemodelindex) + i; };
 	'	// implementation specific call to get a named model
 	'	const studiohdr_t	*FindModel( void **cache, char const *modelname ) const;
-
+	'
 	'	// implementation specific back pointer to virtual data
 	'	mutable void		*virtualModel;
 	'	virtualmodel_t		*GetVirtualModel( void ) const;
-
+	'
 	'	// for demand loaded animation blocks
 	'	int					szanimblocknameindex;	
 	'	inline char * const pszAnimBlockName( void ) const { return ((char *)this) + szanimblocknameindex; }
@@ -223,20 +225,20 @@ Public Class SourceMdlFileData44
 	'	inline mstudioanimblock_t *pAnimBlock( int i ) const { Assert( i > 0 && i < numanimblocks); return (mstudioanimblock_t *)(((byte *)this) + animblockindex) + i; };
 	'	mutable void		*animblockModel;
 	'	byte *				GetAnimBlock( int i ) const;
-
+	'
 	'	int					bonetablebynameindex;
 	'	inline const byte	*GetBoneTableSortedByName() const { return (byte *)this + bonetablebynameindex; }
-
+	'
 	'	// used by tools only that don't cache, but persist mdl's peer data
 	'	// engine uses virtualModel to back link to cache pointers
 	'	void				*pVertexBase;
 	'	void				*pIndexBase;
-
+	'
 	'	// if STUDIOHDR_FLAGS_CONSTANT_DIRECTIONAL_LIGHT_DOT is set,
 	'	// this value is used to calculate directional components of lighting 
 	'	// on static props
 	'	byte				constdirectionallightdot;
-
+	'
 	'	// set during load of mdl data to track *desired* lod configuration (not actual)
 	'	// the *actual* clamped root lod is found in studiohwdata
 	'	// this is stored here as a global store to ensure the staged loading matches the rendering
@@ -250,11 +252,11 @@ Public Class SourceMdlFileData44
 	'	int					unused2[6];		// remove as appropriate
 
 	'	studiohdr_t() {}
-
+	'
 	'private:
 	'	// No copy constructors allowed
 	'	studiohdr_t(const studiohdr_t& vOther);
-
+	'
 	'	friend struct virtualmodel_t;
 	'};
 
