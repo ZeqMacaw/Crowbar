42f40e853e136ae60af66955803082ef8515cf83
ZeqMacaw <zeqmacaw@gmail.com>
2022-08-04 14:59:26 -0400

    Change version to 0.73.

--- Crowbar/My Project/AssemblyInfo.vb
+++ Crowbar/My Project/AssemblyInfo.vb
@@ -33,7 +33,7 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.72.0.0")>
-<Assembly: AssemblyFileVersion("0.72.0.0")>
+<Assembly: AssemblyVersion("0.73.0.0")>
+<Assembly: AssemblyFileVersion("0.73.0.0")>
 
 <Assembly: NeutralResourcesLanguageAttribute("en-US")> 
