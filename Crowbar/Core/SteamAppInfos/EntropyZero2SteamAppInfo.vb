f12a86f6b45bd90a98f828d080990e226b61292d
ZeqMacaw <zeqmacaw@gmail.com>
2023-01-04 08:52:50 -0500

    ADDED: Support for "Entropy : Zero 2" steam workshop.

---
+++ Crowbar/Core/SteamAppInfos/EntropyZero2SteamAppInfo.vb
@@ -0,0 +1,19 @@
+﻿Imports System.ComponentModel
+Imports System.IO
+Imports Steamworks
+
+Public Class EntropyZero2SteamAppInfo
+	Inherits SteamAppInfoBase
+
+	Public Sub New()
+		MyBase.New()
+
+		Me.ID = New AppId_t(1583720)
+		Me.Name = "Entropy : Zero 2"
+		Me.UsesSteamUGC = True
+		Me.CanUseContentFolderOrFile = False
+		'Me.ContentFileExtensionsAndDescriptions.Add("vpk", "Source Engine VPK Files")
+		Me.TagsControlType = GetType(EntropyZero2TagsUserControl)
+	End Sub
+
+End Class
